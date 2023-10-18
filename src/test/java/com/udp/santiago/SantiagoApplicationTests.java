abd999a81627779251bb2b764f6beda349e65497
camilitwo <camilo.igv@gmail.com>
2023-10-18 09:51:12 -0300

    Initial commit

---
+++ src/test/java/com/udp/santiago/SantiagoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udp.santiago;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantiagoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
